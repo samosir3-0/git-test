255ab70c2dd8998403441685455f1f1fd70c0bd5
samosir3-0 <shadowfeedmidplay@gmail.com>
2024-10-07 20:47:07 +0300

    add file

---
+++ git-test.cpp
@@ -0,0 +1,12 @@
+﻿// git-test.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char qwe[] = "Hello world";
+	cout << qwe << endl;
+}
